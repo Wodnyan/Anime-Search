f0a2fc50be4f80c5e86e973d9c28c2c42304cf95
Albert Gergo <gergoalbert.gta@gmail.com>
2020-07-17 12:01:56 +0300

    Setup MongoDB Schema for Users

--- backend/src/db/db.ts
+++ backend/src/db/db.ts
@@ -1,2 +1,29 @@
 import mongoose from "mongoose";
-export default function database(connectLink: string) {}
+import dotenv from "dotenv";
+dotenv.config();
+const URI = `mongodb+srv://wodnyan:${process.env.MONGO_DB}@my-cluster-jv3bc.mongodb.net/anime?retryWrites=true&w=majority`;
+mongoose.connect(
+    URI,
+    { useNewUrlParser: true, useUnifiedTopology: true },
+    (err) => {
+        if (err) return console.error(err);
+        console.log("Connected to mongo");
+    }
+);
+
+const userSchema = new mongoose.Schema({
+    username: {
+        type: String,
+        required: true,
+    },
+    loggedInWith: {
+        type: String,
+        required: true,
+    },
+    providerId: {
+        type: Number,
+        required: true,
+    },
+    profilePicture: String,
+});
+export const User = mongoose.model("users", userSchema);
