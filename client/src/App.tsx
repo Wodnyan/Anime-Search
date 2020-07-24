ee206bdaedd5382244bece103ec2094bd5844c7e
Albert Gergo <gergoalbert.gta@gmail.com>
2020-07-24 12:39:58 +0300

    OAuth login, Message is displayed when logged in and user information is distributed

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,15 +1,86 @@
-import React from "react";
-import { BrowserRouter as Router, Route } from "react-router-dom";
+import React, { useState, useEffect } from "react";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import { CSSTransition } from "react-transition-group";
+import styled from "styled-components";
+import { UserContext } from "./context/UserContext";
+//Views
 import LandingPage from "./components/LandingPage/LandingPage";
 import SearchPage from "./components/SearchPage/SearchPage";
+//Components
+import Message from "./components/Message/Message";
+import { User, UserMessage } from "./interfaces";
+//Style
 import "./App.scss";
 
+const MessageContainer = styled.aside`
+    position: absolute;
+    bottom: 1rem;
+    right: 1rem;
+`;
+
 const App: React.FC = () => {
+    const [messages, setMessages] = useState<UserMessage[] | []>([]);
+    const [user, setUser] = useState<User | null>(null);
+
+    useEffect(() => {
+        async function getUserData() {
+            const httpHeaders = {
+                Accept: "application/json",
+                "Content-Type": "application/json",
+                "Access-Control-Allow-Credentials": "true",
+            };
+            const headers = new Headers(httpHeaders);
+            const authenticate = await fetch(
+                "http://localhost:5050/auth/login",
+                {
+                    method: "GET",
+                    headers: headers,
+                    credentials: "include",
+                }
+            );
+            if (authenticate.status === 200) {
+                const { user } = await authenticate.json();
+                setUser(user);
+                setMessages((prev) => [
+                    ...prev,
+                    {
+                        id: prev.length,
+                        message: `You logged in as ${user.username} with ${user.provider}`,
+                    },
+                ]);
+            } else {
+                console.log("There was an oopsie");
+            }
+        }
+        getUserData();
+    }, []);
+
     return (
-        <Router>
-            <Route exact path="/" component={LandingPage} />
-            <Route path="/search" component={SearchPage} />
-        </Router>
+        <UserContext.Provider value={user}>
+            <Router>
+                <a href="http://localhost:5050/auth/logout">Logout</a>
+                <Switch>
+                    <Route exact path="/" component={LandingPage} />
+                    <Route path="/search" component={SearchPage} />
+                </Switch>
+                <MessageContainer>
+                    <CSSTransition
+                        in={!!messages.length}
+                        classNames="fade"
+                        unmountOnExit
+                        timeout={1000}
+                    >
+                        <React.Fragment>
+                            {(messages as UserMessage[]).map(
+                                ({ message, type, id }) => (
+                                    <Message key={id}>{message}</Message>
+                                )
+                            )}
+                        </React.Fragment>
+                    </CSSTransition>
+                </MessageContainer>
+            </Router>
+        </UserContext.Provider>
     );
 };
 export default App;
