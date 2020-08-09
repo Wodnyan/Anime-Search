f74f7e7be6eda5de236a3929a885f8e1f4c7ef49
Albert Gergo <gergoalbert.gta@gmail.com>
2020-08-09 10:56:23 +0300

    Made an Anime interface

--- client/src/interfaces.ts
+++ client/src/interfaces.ts
@@ -8,3 +8,17 @@ export interface UserMessage {
     message: string;
     type?: "error" | "warning";
 }
+export interface Anime {
+    mal_id: number;
+    url: string;
+    image_url: string;
+    title: string;
+    type: string;
+    episodes: number;
+    airing: boolean;
+    score: number;
+    members: number;
+    synopsis: string;
+    start_date: string;
+    end_date: string;
+}
